abf5904cf2d1c7bf6dc4b4e86d905e0ab5b95ccf
Thomas Vandal <thomas.vandal@umontreal.ca>
2025-12-22 13:00:13 -0500

    Add test notebook

---
+++ tests/test_nb.py
@@ -0,0 +1,15 @@
+# %%
+# First cell
+print("something")
+
+# %%
+# Cell with a function
+def afun(a):
+    if a == 0:
+        print("Zero")
+    else:
+        print("Non-zero")
+
+
+# %%
+print("done")
